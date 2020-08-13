229b9a8b177ad5d51b83ee9aa9bce09c8996d84a
Mathmagician8191 <mathmagician@DESKTOP-H5USK4F>
2020-08-13 14:07:42 +1200

    Piece class for chess

---
+++ src/main/java/com/mathmagician/chessgame/Piece.java
@@ -0,0 +1,15 @@
+package chess;
+
+class Piece {
+  boolean isPiece;
+  boolean side; //white=true
+  char letter; //used to convert to FEN and to see legal moves
+  public Piece(char letter) {
+    this.letter = Character.toLowerCase(letter);
+    this.isPiece = true;
+    this.side = Character.toUpperCase(letter)==letter;
+  }
+  public Piece() {
+    this.isPiece = false; //for empty squares
+  }
+}
